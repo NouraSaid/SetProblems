2ce71ae1bb599b780c470af0310dcad663f6ff6c
NouraSaid <noura.alsaidi98@gmail.com>
2024-02-26 12:32:28 +0400

    CreateSet

---
+++ CreateSet.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+  
+public class CreateSet { 
+    public static void main(String args[]) 
+    { 
+
+        Set<Integer> set = new HashSet<Integer>(); // Creating an empty Set 
+  
+        // Use add() method to add elements into the Set 
+        set.add(12); 
+        set.add(13); 
+        set.add(14); 
+        set.add(15); 
+        set.add(16); 
+  
+        System.out.println("Set: " + set); //print out the set values
+        
+  
+      
+    } 
+} 
