71548bb1394681cd225604a2d64300dd50707d14
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-28 09:57:53 +0400

    Update CheckDisjointSets.java

--- CheckDisjointSets.java
+++ CheckDisjointSets.java
@@ -1,27 +1,22 @@
 import java.util.HashSet;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.Collections;
 
 public class CheckDisjointSets {
     public static void main(String[] args) {
         
-        HashSet<String> set1 = new HashSet<>(); //1 Set
-        HashSet<String> set2 = new HashSet<>(); // 2 Set
+        HashSet<String> set1 = new HashSet<>(); // Set 1
+        HashSet<String> set2 = new HashSet<>(); // Set 2
        
+        // Add elements for each Sets
+        set1.addAll(Arrays.asList("NOURA", "SAID", "MOHAMMED")); 
+        System.out.println("Set1: " + set1);
         
-        //add elements for each Sets
-        set1.addAll(Arrays.asList("NOURA","SAID","MOHAMMED")); 
-        System.out.println("\nSet1: " + set1);
-        set2.addAll(Arrays.asList("Abdullah","AFNAN","TAYBA"));
-        System.out.println("\nSet1: " + set2);
-        
+        set2.addAll(Arrays.asList("Abdullah", "AFNAN", "TAYBA", "NOURA"));
+        System.out.println("Set2: " + set2);
 
-        
-      // Check if the sets are disjoint
-        HashSet<String> intersection = new HashSet<>(set1);
-        intersection.retainAll(set2);
-
-        if (intersection.isEmpty()) {
+        // Check if the sets are disjoint using Collections.disjoint()
+        if (Collections.disjoint(set1, set2)) {
             System.out.println("Disjoint");
         } else {
             System.out.println("Not Disjoint");
